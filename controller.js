732c9ce4fdf4cc384642d1eba149868bd44f24f7
thesaam <samuel@wedeydo.com>
2024-05-29 15:45:08 +0100

    feat: add and remove from order quantity

---
+++ controller.js
@@ -0,0 +1,26 @@
+const quantity = document.getElementById("quantity");
+const minusQuantity = document.getElementById("minus");
+const addQuantity = document.getElementById("plus");
+
+
+// functions
+let amount = quantity.textContent;
+
+const updateQuantity = function () {
+  quantity.innerText = amount
+}
+
+console.log(amount)
+
+minusQuantity.addEventListener('click', function() {
+  if (amount === 0) {
+    return;
+  }
+  amount--
+  updateQuantity()
+})
+
+addQuantity.addEventListener('click', function() {
+  amount++
+  updateQuantity()
+})
