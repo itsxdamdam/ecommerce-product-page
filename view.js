f29548b78539d989bbc77caffb93373797b35776
thesaam <samuel@wedeydo.com>
2024-06-04 00:02:49 +0100

    fix: more cart container visibility

--- view.js
+++ view.js
@@ -21,23 +21,51 @@ cartIcon.addEventListener('click', function(e) {
 window.addEventListener("click", function(e) {
   const cartVisibility = cartList.classList.contains("hidden")
 
-  if(!cartVisibility && e.target !== cartIcon) {
-    const displayItem = document.getElementById("display-item")
+  let cartListElements = [cartList]
+  cartListElements.push(...cartList.getElementsByTagName("*"))
 
-    if(e.target.parentElement === cartList || e.target.parentElement === cart) {
-      cartList.classList.remove('hidden')
-    } else if(e.target.parentElement === displayItem) {
-      cartList.classList.remove('hidden')
-    } else if(e.target.parentElement.parentElement === displayItem) {
-      cartList.classList.remove('hidden')
-    } else if(e.target.parentElement.parentElement.parentElement === displayItem) {
-      cartList.classList.remove('hidden')
-    } else if(e.target.parentElement.parentElement.parentElement.parentElement === displayItem) {
-      cartList.classList.remove('hidden')
-    } else {
-      cartList.classList.add('hidden')
+  if(!cartVisibility && e.target !== cartIcon) {
+
+    cartList.classList.add('hidden')
+    
+    for(let i = 0; i < cartListElements.length; i++) {
+      
+      if(e.target === cartListElements[i]) {
+       
+        // console.log(e.target, cartListElements[i])
+       
+        if(true) {
+          cartList.classList.remove('hidden')
+        }
+      }
     }
+    
   }
+
+  
+  if(cartListElements) {}
+
+  // if(!cartVisibility && e.target !== cartIcon) {
+  //   const displayItem = document.getElementById("display-item")
+
+  //   if(e.target.parentElement === cartList || e.target.parentElement === cart) {
+  //     cartList.classList.remove('hidden')
+  //   } else if(e.target.parentElement === displayItem) {
+  //     cartList.classList.remove('hidden')
+  //   } else if(e.target.parentElement.parentElement === displayItem) {
+  //     cartList.classList.remove('hidden')
+  //     console.log("error not here")
+
+  //   } else if(e.target.parentElement.parentElement.parentElement === displayItem) {
+  //     console.log("error here")
+  //     console.log(e.target.parentElement.parentElement.parentElement)
+  //     cartList.classList.remove('hidden')
+  //   } else if(e.target.parentElement.parentElement.parentElement.parentElement === displayItem) {
+  //     cartList.classList.remove('hidden')
+  //   } else {
+  //     cartList.classList.add('hidden')
+  //   }
+  // }
 })
 
 addToCart.addEventListener("click", function() {
