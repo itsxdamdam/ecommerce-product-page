f377b087e12e1fa2937062fde329c98fb4c4f62f
thesaam <samuel@wedeydo.com>
2024-06-07 20:33:22 +0100

    fix: mobilenav buttons and background when active

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 const navbar = document.querySelector(".navbar")
 const hamburger = document.querySelector(".hamburger-menu")
 const navClose = document.querySelector(".nav-close")
-const body = document.querySelector("body")
+const bodyDark = document.querySelector(".body-dark")
 
 hamburger.addEventListener("click", function() {
   navbar.classList.add("mobile-nav");
   navClose.classList.remove("hidden");
-  console.log(body)
-  body.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-  body.style.zIndex = 10000
+  bodyDark.classList.remove("hidden")
+  // body.style.backgroundColor = "rgba(0, 0, 0, 1)";
+  // body.style.zIndex = 10000
 })
 
 navClose.addEventListener("click", function() {
   navbar.classList.remove("mobile-nav")
   navClose.classList.add("hidden")
+  bodyDark.classList.add("hidden")
 })
