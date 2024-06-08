c47d656736f84ee555492b3d6cab8cfe3ab1ad13
thesaam <samuel@wedeydo.com>
2024-06-08 01:24:53 +0100

    refactor: fix for hamburger links on mobile screens

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const navbar = document.querySelector(".navbar")
 const hamburger = document.querySelector(".hamburger-menu")
 const navClose = document.querySelector(".nav-close")
 const bodyDark = document.querySelector(".body-dark")
+const hamburgerLinks = document.querySelector(".hamburger-links")
 
 hamburger.addEventListener("click", function() {
   navbar.classList.add("mobile-nav");
   navClose.classList.remove("hidden");
   bodyDark.classList.remove("hidden")
+  hamburgerLinks.classList.add("mobile-nav-link")
 })
 
 navClose.addEventListener("click", function() {
